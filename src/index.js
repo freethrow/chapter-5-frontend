c4e2502090cf8acaec0442d15fcb9e5777f7f721
freethrow <aleksendric@gmail.com>
2022-05-03 19:23:11 +0200

    added router and basic pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+
+import Car from './pages/Car'
+import Cars from './pages/Cars'
+import NewCar from './pages/NewCar'
+import EditCar from './pages/EditCar'
+import About from './pages/About'
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="cars" element={<Cars />} />
+        <Route path="cars/new" element={<NewCar />} />
+        <Route path="cars/edit/:id" element={<EditCar />} />
+        <Route path="cars/:id" element={<Car />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
